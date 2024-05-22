bbabecf202cd980caba5f59e25d7d47cfffef142
Pawan Yadav <pawan2225ydv@gmail.com>
2024-05-23 00:28:49 +0530

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import "../css/home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="main flex">
+      <div className=" container-div w-1/2 border-2 border-blue-500"></div>
+          <div className=" container-div w-1/2 border-2 border-red-900 "></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
