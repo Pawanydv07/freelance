38c09fb3868f7b0e6aa591028fbcb4d6de2153fb
Pawan Yadav <pawan2225ydv@gmail.com>
2024-05-23 07:28:30 +0530

    Home page
    Home page Navbar and right div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <div className="links">
+      <div className="links flex">
         <Link to="/">Home</Link>
         <Link to="/pages">Pages</Link>
         <Link to="/classes">Classes</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React from 'react';
 import "../css/home.css";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import photo from "../assets/photo.jpg";
 const Home = () => {
   return (
-    <div className="main flex">
-      <div className=" container-div w-1/2 border-2 border-blue-500"></div>
-          <div className=" container-div w-1/2 border-2 border-red-900 "></div>
+    <div className='main flex'>
+      <div className='left-half'></div>
+      <div className='right-half'>
+        <img src={photo}alt="photo" className='photo'/>
+        <div className='navbar flex'>
+        <div className='menu'>
+            <div className='hamburger'>
+              <div className='line small'></div>
+              <div className='line big'></div>
+              <div className='line small'></div>
+            </div>
+            <span>Menu</span>
+          </div>
+          <div className='icons-right'>
+            <FontAwesomeIcon icon={faSearch} className='icon' />
+            <FontAwesomeIcon icon={faShoppingCart} className='icon' />
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
