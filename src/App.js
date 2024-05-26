ade15a1863325642b2242d7c09933e7bfa5c2539
ramgopal <ramgopalbagh009@gmail.com>
2024-05-26 14:30:02 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { About, Contact, Home, Services } from "./pages";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,33 @@
-import React from 'react';
-import "../css/home.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import photo from "../assets/photo.jpg";
-const Home = () => {
+import React from "react";
+import Slider from "../components/Slider";
+import "../css/Home.css"
+import sliderimage1 from "../assets/photo1.jpg";
+import sliderimage2 from "../assets/photo2.jpg";
+import sliderimage3 from "../assets/photo3.jpg";
+
+export const Home = () => {
+  const slides = [
+    {
+      image: sliderimage1,
+      title: "100% placement",
+      maintitle:"Welcome to Our Website",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet semper libero, in volutpat justo lacinia id. Sed ultrices euismod turpis, non blandit lacus."
+    },
+    {
+      image: sliderimage2,
+      title: "Explore Our Services",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet semper libero, in volutpat justo lacinia id. Sed ultrices euismod turpis, non blandit lacus."
+    },
+    {
+      image: sliderimage3,
+      title: "Contact Us Today",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet semper libero, in volutpat justo lacinia id. Sed ultrices euismod turpis, non blandit lacus."
+    }
+  ];
+
   return (
-    <>
-    <div className='main flex'>
-      <div className='left-half'></div>
-      <div className='right-half'>
-        <img src={photo}alt="photo" className='photo'/>
-        <div className='navbar flex'>
-        <div className='menu'>
-            <div className='hamburger'>
-              <div className='line small'></div>
-              <div className='line big'></div>
-              <div className='line small'></div>
-            </div>
-            <span>Menu</span>
-          </div>
-          <div className='icons-right'>
-            <FontAwesomeIcon icon={faSearch} className='icon' />
-            <FontAwesomeIcon icon={faShoppingCart} className='icon' />
-          </div>
-        </div>
-      </div>
-    
+    <div className="home-container">
+      <Slider slides={slides} />
     </div>
-    <div className='choose'>
-        <div className='choose-us text-center font-bold text-black text-lg mt-10'>
-          <p>Why Choose US</p>
-          <p className='benefits'>BENEFITS OF THIS</p>
-          <p className='courses'>COURSES</p>
-        </div>
-
-        </div>
-    </>
   );
-}
-
-export default Home;
+};
