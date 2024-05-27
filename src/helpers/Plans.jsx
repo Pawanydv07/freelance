b46d534c5e15a5167d15c8b9da277a7ed42ae1b4
Pawan Yadav <pawan2225ydv@gmail.com>
2024-05-27 12:09:42 +0530

    update boxes

--- src/helpers/Plans.jsx
+++ src/helpers/Plans.jsx
@@ -3,13 +3,14 @@ import "../css/plans.css";
 
 const Plans = () => {
   return (
-    <div className="main-container justify-center mt-20 bg-orange-200 relative">
+    <div className="main-container justify-center relative">
       <div className="text-center">
         <h1 className="text-6xl font-bold plan-text">
           Choose <span className="gradient-text">Your Plan</span>
         </h1>
+        
       </div>
-      <div className="flex gap-16 justify-center">
+      <div className="flex gap-16 justify-center mt-10">
         <div className="choose-1 border-2 border-gray-900"></div>
         <div className="choose-1 border-2 border-gray-900"></div>
         <div className="choose-1 border-2 border-gray-900"></div>
