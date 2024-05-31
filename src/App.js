446751adfc77fe14d4df9e10885e8cebd1a8c7fb
ramgopal <ramgopalbagh009@gmail.com>
2024-06-01 02:29:48 +0530

    Merge branch 'master' of https://github.com/Pawanydv07/freelance

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import { Navbar } from "./components/Navbar";
 import { About, Contact, Home, Services } from "./pages";
-import Plans from "./helpers/Plans";
 
 function App() {
   return (
     <div className="App">
-      {/* <Plans/> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/plans"element={<Plans/>}/>
       </Routes>
     </div>
   );
